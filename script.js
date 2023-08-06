5c67141992de17130d03ecc06a914c789cd0b6a2
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-06 13:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname = document.getElementsByTagName("input")[0].value;
+	let lname = document.getElementsByTagName("input")[1].value;
+	let submit = document.getElementsByTagName("input")[2];
+	alert(fname + " " + lname);
 }
